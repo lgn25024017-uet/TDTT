b57cd1dff618b72aa72e017584bda9ffda968258
le gia nguyen-25024017 <lgn260407@gmail.com>
2026-03-12 11:21:55 +0700

    Clean up gitignore

--- em 2.py
+++
@@ -1,9 +0,0 @@
-
-name = "Alice"
-age = 30
-
-# Without f-string
-message = "My name is " + name + " and I am " + str(age) + " years old."
-
-
-print(message)  
--- sam_ple.py
+++
@@ -1,6 +0,0 @@
-print("Hello, World!" + " Welcome to Python programming." + " Let's learn together!")
-def move_forward():
-    print("Moving forward") 
-move_forward()
-print ("123" + "456")
-print("Hello, World!" + " Welcome to Python programming." + " Let's learn together!")
